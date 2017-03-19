ff31efb59fae19368d95dac70574cf42f132928c
Samuel Jones <samuel.lucyk.jones@gmail.com>
2017-03-19 12:57:24 +0100

    typo

--- build_commit_pub_list.sh
+++ build_commit_pub_list.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+new_ads_query=false
+
 pubfile="./publications.md"
 pubdir="/home/swj/Documents/publication-list/"
 publist=$pubdir"publications.list"
 pubquery=$pubdir"ads_api_pub_list.py"
 
 # query ads for new list sing orcid
-python2.7 $pubquery
+
+# ...do something interesting...
+if [ "$new_ads_query" = true ] ; then
+	python2.7 $pubquery
+fi
+
 
 # write new pub list
 echo "---" > $pubfile
 echo "layout: page" >> $pubfile
 echo "title: Publications" >> $pubfile
 echo "permalink: /publications/" >> $pubfile
-echo "---" > $pubfile
+echo "---" >> $pubfile
 cat $publist >> $pubfile
 
 # commit and push to github
